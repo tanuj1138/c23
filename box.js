7b4cc2b55c5e812da5a1e257d72197ce4977baaf
tanuj1138 <68948157+tanuj1138@users.noreply.github.com>
2021-02-24 04:06:32 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+class Box {
+
+    constructor(x,y,width,height){
+        
+        var option= {'restitution':1}
+        this.body = Bodies.rectangle(x,y,width,height,option);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+
+    display(){
+     var pos = this.body.position;
+
+     rectMode(CENTER);
+     fill(255);
+     rect(pos.x,pos.y,this.width,this.height);   
+
+    }
+}
